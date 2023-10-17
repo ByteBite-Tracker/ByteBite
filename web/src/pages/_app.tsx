054ca9081d30f0226b7b04064643ee2d344cddd3
Sebastian Gora <gora1s@cmich.edu>
2023-10-17 12:32:54 -0400

    moved navbar again

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
+import { Navbar } from "@/components/navbar/navbar";
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
+import { Client, Provider, cacheExchange, fetchExchange } from "urql";
+import { SelectedPage } from "./shared/types";
 
 const client = new Client({
-  url: 'http://127.0.0.1:8000/graphql',
+  url: "http://127.0.0.1:8000/graphql",
   exchanges: [cacheExchange, fetchExchange],
 });
 
@@ -15,11 +19,21 @@ const client = new Client({
 // });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const route = useRouter();
+  const [selectedPage, setSelectedPage] = useState(SelectedPage.Home);
   return (
-    <Provider value={client}>
-      <ChakraProvider>
-       <Component {...pageProps} />
-      </ChakraProvider>
-    </Provider>
-  )
+    <>
+      <Navbar
+        isTopOfPage={true}
+        setSelectedPage={setSelectedPage}
+        selectedPage={selectedPage}
+        children={undefined}
+      />
+      <Provider value={client}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Provider>
+    </>
+  );
 }
